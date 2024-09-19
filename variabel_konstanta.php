ef1efec4d8bb048a41dec87f272e27524a1a4a83
saalsabl <aisyahnzkk@gmail.com>
2024-09-19 08:42:36 +0700

    Praktikum 1

--- variabel_konstanta.php
+++ variabel_konstanta.php
@@ -2,10 +2,10 @@
 $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
-echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil. <br>";
 $benar = true;
 $salah = false;
-echo "Variabel benar: $benar, Variabel salah: $salah";
+echo "Variabel benar: $benar, Variabel salah: $salah <br>";
 //Mendefinisikan konstanta untuk nilai tetap
 define("NAMA_SITUS", "WebsiteKu.com");
 define("TAHUN_PENDIRIAN", 2023);
