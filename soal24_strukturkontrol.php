70c4787ff7e90eb0ca17d63d46dbcdcd36d04475
saalsabl <aisyahnzkk@gmail.com>
2024-09-20 22:52:31 +0700

    Soal no24 Struktur Kontrol

---
+++ soal24_strukturkontrol.php
@@ -0,0 +1,17 @@
+<?php
+$hargaProduk = 12000;
+$diskon = 0.2;
+
+$minimumPembelian = 10000;
+$hargaSebelumDiskon = $hargaProduk;
+
+if ($hargaProduk >= $minimumPembelian) {
+    $nilaiDiskon = $hargaProduk * $diskon;
+    $hargaSetelahDiskon = $hargaProduk - $nilaiDiskon;
+} else {
+    $hargaSetelahDiskon = $hargaSebelumDiskon;
+}
+
+echo "Harga Produk : Rp " . number_format($hargaProduk) . "<br>";
+echo "Harga Sebelum Diskon : Rp " . number_format($hargaSebelumDiskon) . "<br>";
+echo "Diskon: " . $diskon * 100 . "% (Rp" . number_format($nilaiDiskon) . ")<br>";
