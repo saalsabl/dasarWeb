aa7b8374a6f35b16fa5b955b64c08f8fa8b613f3
saalsabl <aisyahnzkk@gmail.com>
2024-09-20 23:02:31 +0700

    Soal no25 Struktur Kontrol

---
+++ soal25_strukturkontrol.php
@@ -0,0 +1,9 @@
+<?php
+$poin = 700; //Misal, ganti dengan nilai skor sesuai kebutuhan
+
+$hadiahTambahan = ($poin > 500) ? "YA" : "TIDAK";
+
+echo "Total skor pemain adalah : $poin <br>";
+echo "Apakah pemain mendapatkan hadiah tambahan? $hadiahTambahan <br>";
+
+?>
