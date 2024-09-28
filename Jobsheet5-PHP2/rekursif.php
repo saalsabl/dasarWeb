6df2c6abd9a2e770cf3fcf76d7fd2e0aa92779f1
saalsabl <aisyahnzkk@gmail.com>
2024-09-28 11:12:16 +0700

    Fungsi Rekursif

--- Jobsheet5-PHP2/rekursif.php
+++ Jobsheet5-PHP2/rekursif.php
@@ -1,13 +1,12 @@
 <?php
-for ($i=1; $i<=25; $i++) { 
-    echo "Perulangan ke-{$i}<br>";
+function tampilkanAngka (int $jumlah, int $indeks = 1){
+    echo "Perulangan ke-{$indeks} <br>";
+
+    //panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
 }
 
-function tampilkanHaloDunia(){
-    echo "Halo dunia! <br>";
-
-    tampilkanHaloDunia();
-}
-
-tampilkanHaloDunia();
+tampilkanAngka(20);
 ?>
