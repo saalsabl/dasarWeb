a4922e44f29c7f21f9ad3c60f11705d152a963ad
saalsabl <aisyahnzkk@gmail.com>
2024-09-28 09:57:44 +0700

    Memangil Funsi di dalam fungsi

--- Jobsheet5-PHP2/fungsi.php
+++ Jobsheet5-PHP2/fungsi.php
@@ -5,5 +5,14 @@ function hitungUmur($thn_lahir, $thn_sekarang){
    return $umur;
 }
 
-echo "Umur saya adalah ".hitungUmur(2003, 2024)."tahun";
+function perkenalan ($nama, $salam="Assalamualaikum "){
+    echo $salam.",";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+
+    //memanggil fungsi lain
+    echo "Saya berusia ". hitungUmur(2003, 2024)." tahun<br/>";
+    }
+
+    //memanggil fungsi perkenalan
+    perkenalan("Icha");
 ?>
