581c58c9bb1c049ec7ea129f64b488416ced5517
saalsabl <aisyahnzkk@gmail.com>
2024-09-28 11:29:43 +0700

    Menu Bertingkat

--- Jobsheet5-PHP2/rekursif.php
+++ Jobsheet5-PHP2/rekursif.php
@@ -1,12 +1,43 @@
 <?php
-function tampilkanAngka (int $jumlah, int $indeks = 1){
-    echo "Perulangan ke-{$indeks} <br>";
-
-    //panggil diri sendiri selama $indeks <= $jumlah
-    if ($indeks < $jumlah) {
-        tampilkanAngka($jumlah, $indeks + 1);
+$menu = [
+    [
+            "nama" => "Beranda"
+    ],
+    [
+    "nama" => "Berita",
+    "subMenu" => [
+        [
+            "nama" => "Wisata",
+            "subMenu" => [
+                [
+                    "nama" => "Pantai"
+                ],
+                [
+                    "nama" => "Gunung"
+                ]
+            ]
+        ],
+        [
+            "nama" => "Kuliner"  
+        ],
+        [
+        "nama" => "Hiburan"
+        ]
+    ]
+],
+        [
+            "nama" => "Tentang"
+        ],
+        [
+           "nama" => "Kontak" 
+        ],
+];
+    function tampilkanMenuBertingkat (array $menu) {
+        echo "<ul>";
+        foreach($menu as $key => $item){
+            echo "<li>{$item['nama']}</li>";
+        }
+        echo "</ul>";
     }
-}
-
-tampilkanAngka(20);
+    tampilkanMenuBertingkat($menu);
 ?>
