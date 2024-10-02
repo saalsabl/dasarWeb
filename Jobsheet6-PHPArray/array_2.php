471916655dc6822bfdfde90135371dd75871aea9
saalsabl <aisyahnzkk@gmail.com>
2024-10-02 21:09:02 +0700

    Praktikum 2

---
+++ Jobsheet6-PHPArray/array_2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <?php
+        $Dosen = [
+            'nama' => 'Elok Nur Hamdana',
+            'domisili' => 'Malang',
+            'jenis kelamin' => 'Perempuan' ];
+        echo "Nama : {$Dosen ['nama']} <br>";
+        echo "Domisili : {$Dosen ['domisili']} <br>";
+        echo "Jenis Kelamin : {$Dosen ['jenis_kelamin']} <br>";
+    ?>
+</body>
+</html>
