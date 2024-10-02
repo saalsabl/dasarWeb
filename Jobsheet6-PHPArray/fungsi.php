0b5b7d8cb69de1fc3d603a1a2843ec5d0018d32e
saalsabl <aisyahnzkk@gmail.com>
2024-10-02 21:09:37 +0700

    Fungsi

---
+++ Jobsheet6-PHPArray/fungsi.php
@@ -0,0 +1,10 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Icha<br/>";
+    echo "Senang berkanalan dengan Anda<br/>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+?>
