ad2a5b490804d61ef860a001f760351ab40f8250
saalsabl <aisyahnzkk@gmail.com>
2024-10-03 08:20:06 +0700

    Menu bertingkat

---
+++ Jobsheet6-PHPArray/menu.php
@@ -0,0 +1,58 @@
+<?php
+$menu = [
+    [
+            "nama" => "Beranda"
+    ],
+    [
+    "nama" => "Berita",
+    "subMenu" => [
+        [
+            "nama" => "Wisata",
+            "subMenu" => [
+                [
+                    "nama" => "Pantai"
+                ],
+                [
+                    "nama" => "Gunung"
+                ]
+            ]
+        ],
+        [
+            "nama" => "Kuliner"  
+        ],
+        [
+        "nama" => "Hiburan"
+        ]
+    ]
+],
+        [
+            "nama" => "Tentang"
+        ],
+        [
+           "nama" => "Kontak" 
+        ],
+];
+    // function tampilkanMenuBertingkat (array $menu) {
+    //     echo "<ul>";
+    //     foreach($menu as $key => $item){
+    //         echo "<li>{$item['nama']}</li>";
+    //     }
+    //     echo "</ul>";
+    // }
+    // tampilkanMenuBertingkat($menu);
+
+    function tampilkanMenuBertingkat(array $menu){
+        echo "<ul>";
+        foreach ($menu as $item) {
+            echo "<li>{$item['nama']}";
+            if (isset($item['subMenu'])) {
+                tampilkanMenuBertingkat($item['subMenu']);
+            }
+            echo "</li>";
+        }
+        echo "</ul>";
+    }
+    
+    tampilkanMenuBertingkat($menu);
+    
+?>
