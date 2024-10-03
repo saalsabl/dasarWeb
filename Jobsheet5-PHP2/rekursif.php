60141dba8f1f5d92e726659b31583593cfdf93d1
saalsabl <aisyahnzkk@gmail.com>
2024-10-03 08:19:47 +0700

    perbaikan

--- Jobsheet5-PHP2/rekursif.php
+++ Jobsheet5-PHP2/rekursif.php
@@ -32,12 +32,27 @@ $menu = [
            "nama" => "Kontak" 
         ],
 ];
-    function tampilkanMenuBertingkat (array $menu) {
+    // function tampilkanMenuBertingkat (array $menu) {
+    //     echo "<ul>";
+    //     foreach($menu as $key => $item){
+    //         echo "<li>{$item['nama']}</li>";
+    //     }
+    //     echo "</ul>";
+    // }
+    // tampilkanMenuBertingkat($menu);
+
+    function tampilkanMenuBertingkat(array $menu){
         echo "<ul>";
-        foreach($menu as $key => $item){
-            echo "<li>{$item['nama']}</li>";
+        foreach ($menu as $item) {
+            echo "<li>{$item['nama']}";
+            if (isset($item['subMenu'])) {
+                tampilkanMenuBertingkat($item['subMenu']);
+            }
+            echo "</li>";
         }
         echo "</ul>";
     }
+    
     tampilkanMenuBertingkat($menu);
+    
 ?>
