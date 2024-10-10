c4e3dc13b73abf97729f6f76f89dbd70d06c77bc
saalsabl <aisyahnzkk@gmail.com>
2024-10-10 09:45:54 +0700

    Function Empty 2.2

--- Jobsheet7-FormProccessing/empty.php
+++ Jobsheet7-FormProccessing/empty.php
@@ -1,7 +1,13 @@
 <?php
 $myArray = array(); //Array kosong
 if (empty($myArray)) {
-    echo "Array tidak terdefinisi atau kosong.";
+    echo "Array tidak terdefinisi atau kosong.<br>";
 } else {
     echo "Array terdefinisi dan tidak kosong.";
+}
+
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.<br>";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong";
 }
