cb7755965359824a87a98b12eec43fcf69a944c6
saalsabl <aisyahnzkk@gmail.com>
2024-10-12 12:01:22 +0700

    Praktikum 4 : HTML Injection 4.2

--- Jobsheet7-FormProccessing/html_aman.php
+++ Jobsheet7-FormProccessing/html_aman.php
@@ -2,15 +2,21 @@
 if (isset($_POST['submit'])){
     $input = $_POST['input'];
     $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8'); 
+    $email = $_POST['email'];
 }
 ?>
 <form action="" method="post">
     <input type="text" name="input" id="input" value="" placeholder="nama">
+    <input type="text" name="email" id="email" placeholder="email">
     <input type="submit" name="submit" value="Submit">
     <br>
     <br>
-    <?php
-    if (isset($_POST['submit'])) 
-        echo $input;
+<?php
+if (isset($_POST['submit'])) 
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Email valid";
+    } else {
+        echo "Email tidak valid";
+    }
 ?>
 </form>
