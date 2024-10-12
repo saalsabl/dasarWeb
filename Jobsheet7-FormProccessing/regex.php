16dd253ede399b36458590cc7e649ce30a7859da
saalsabl <aisyahnzkk@gmail.com>
2024-10-12 12:15:54 +0700

    Praktikum 5 : Penggunaan Regex Pada PHP 5.1

---
+++ Jobsheet7-FormProccessing/regex.php
@@ -0,0 +1,9 @@
+<?php
+$pattern = '/[a-z]/'; // Cocokkan huruf kecil.
+$text = 'This is a Sample Text.';
+if (preg_match($pattern, $text)) {
+    echo "Huruf kecil ditemukan!";
+} else {
+    echo "<br>Tidak ada huruf kecil!";
+}
+
