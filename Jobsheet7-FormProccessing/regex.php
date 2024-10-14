28da818ab65db0145cb898dcba71d0c624f0c4ad
saalsabl <aisyahnzkk@gmail.com>
2024-10-14 18:03:34 +0700

    Praktikum 5 : Penggunaan Regex Pada PHP 5.4

--- Jobsheet7-FormProccessing/regex.php
+++ Jobsheet7-FormProccessing/regex.php
@@ -20,3 +20,11 @@ $replacement = 'banana';
 $text = '<br> I Like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text; // output: "I Like banana pie."
+
+$pattern = '/go*d/'; // Cocokkan "god", "good", "gooood", dll.
+$text = 'god is good,';
+if (preg_match($pattern, $text, $matches)) {
+    echo "<br> Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
