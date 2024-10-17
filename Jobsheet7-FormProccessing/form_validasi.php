38ba7cab2d93f11947af5632b6f2f7db0ab2afc3
saalsabl <aisyahnzkk@gmail.com>
2024-10-17 07:25:18 +0700

    Praktikum 7 : Validasi Form 7.1

---
+++ Jobsheet7-FormProccessing/form_validasi.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Input dengan Validasi</title>
+    
+</head>
+<body>
+    <h1>Form Input dengan Validasi</h1>
+    <form method="post" action="proses_validasi.php">
+        <label for="nama">Nama : </label>
+        <input type="text" id="nama" name="nama">
+        <br>
+
+        <label for="email">Email : </label>
+        <input type="text" id="email" name="email">
+        <br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
---
+++ Jobsheet7-FormProccessing/proses_validasi.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nama = $_POST["nama"];
+    $email = $_POST["email"];
+    $errors = array();
+
+    //Validasi nama
+    if (empty($nama)) {
+        $errors[] = "Nama harus diisi.";
+    }
+
+    //Validasi email
+    if (empty($email)) {
+        $errors[] = "Email harus diisi.";
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Format email tidak valid.";
+    }
+
+    //Jika ada kesalahan validasi
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo $error . "<br>";
+        }
+    } else {
+        // lanjutkan dengan pemrosesan data jika semua validasi berhasil
+        // misalnya, menyimpan data ke database atau mengirim email
+        echo "Data berhasil dikirim: Nama = $nama, Email = $email";
+    }
+}
