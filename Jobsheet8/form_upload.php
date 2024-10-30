dc4459d308b345564f128758e960be52cd3a2e27
saalsabl <aisyahnzkk@gmail.com>
2024-10-30 20:29:00 +0700

    Praktikum 1 no 1-2

--- Jobsheet8/form_upload.php
+++ Jobsheet8/form_upload.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File Upload</title>
 </head>
 <body>
@@ -11,4 +9,4 @@
         <input type="submit" name="submit">
     </form>
 </body>
-</html>
+</html> 
--- Jobsheet8/upload.php
+++ Jobsheet8/upload.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST["submit"])) {
     $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
-    $targetfile = $targetdir . basename(($_FILES["myfile"]["name"]));
+    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
 
     if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
         echo "File berhasil diunggah.";
